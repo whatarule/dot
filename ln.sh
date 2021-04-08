df0d3c49949ea09e219d5f5e601c423f314edc45
wk <whatarule@gmail.com>
2021-04-08 15:46:50 +0900

    ln

---
+++ ln.sh
@@ -0,0 +1,6 @@
+
+ln -fns ~/wk/dot/.vimrc ~/.config/nvim/init.vim
+ln -fns ~/wk/dot/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json 
+
+ln -fns ~/wk/dot/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -fns ~/wk/dot/.vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
