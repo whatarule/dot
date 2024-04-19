537667f62f1d62989a4c3c0aee4f8025413be7b0
KOMAGATA, Wataru <whatarule@gmail.com>
2024-04-20 07:29:31 +0900

    fish: comment out customised cd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,10 +35,10 @@ set -x SBT_OPTS '-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:Max
 set -x FZF_DEFAULT_COMMAND 'find .'
 set -U FZF_LEGACY_KEYBINDINGS 0
 
-function cd
-  builtin cd $argv
-  ls
-end
+#function cd
+#  builtin cd $argv
+#  ls
+#end
 
 #function source
 #  source HOME/.config/fish/config.fish
